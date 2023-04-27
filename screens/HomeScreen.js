6d35c0f7729506435a6617b2c31467c199e89223
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2023-04-28 00:03:51 +0700

    Implementasi expo-geolocation untuk mendapatkan lokasi pengguna

---
+++ screens/HomeScreen.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+import { Alert, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import * as Location from "expo-location";
+
+const HomeScreen = () => {
+  const [displayCurrentAddress, setdisplayCurrentAddress] = useState(
+    "we are loading your location"
+  );
+  const [locationServicesEnabled, setlocationServicesEnabled] = useState(false);
+
+  useEffect(() => {
+    checkIfLocationEnabled();
+    getCurrentLocation();
+  }, []);
+
+  const checkIfLocationEnabled = async () => {
+    let enabled = await Location.hasServicesEnabledAsync();
+    if (!enabled) {
+      Alert.alert(
+        "Location services not enabled",
+        "Please enable the location services",
+        [
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel",
+          },
+          { text: "OK", onPress: () => console.log("OK Pressed") },
+        ],
+        { cancelable: false }
+      );
+    } else {
+      setlocationServicesEnabled(enabled);
+    }
+  };
+
+  const getCurrentLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+
+    if (status !== "granted") {
+      Alert.alert(
+        "Permission denied",
+        "allow the app to use the location services",
+        [
+          {
+            text: "Cancel",
+            onPress: () => console.log("Cancel Pressed"),
+            style: "cancel",
+          },
+          { text: "OK", onPress: () => console.log("OK Pressed") },
+        ],
+        { cancelable: false }
+      );
+    }
+
+    const { coords } = await Location.getCurrentPositionAsync();
+    if (coords) {
+      const { latitude, longitude } = coords;
+
+      let response = await Location.reverseGeocodeAsync({
+        latitude,
+        longitude,
+      });
+
+      for (let item of response) {
+        let address = `${item.name} ${item.city} ${item.postalCode}`;
+        setdisplayCurrentAddress(address);
+      }
+    }
+  };
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>{displayCurrentAddress}</Text>
+      </View>
+      <Text>HomeScreen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({});
