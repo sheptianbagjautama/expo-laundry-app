b0a5769b1e7bb90b5e6c3397b47640d08e267a7a
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2023-05-03 11:26:07 +0700

    clean code styling login screen

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -16,32 +16,15 @@ const LoginScreen = () => {
   const [password, setPassword] = useState("");
 
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        backgroundColor: "white",
-        alignItems: "center",
-        padding: 10,
-      }}
-    >
+    <SafeAreaView style={styles.containerSafeArea}>
       <KeyboardAvoidingView>
-        <View
-          style={{
-            justifyContent: "center",
-            alignItems: "center",
-            marginTop: 100,
-          }}
-        >
-          <Text style={{ fontSize: 20, color: "#662d91", fontWeight: "bold" }}>
-            Sign In
-          </Text>
-          <Text style={{ fontSize: 18, marginTop: 8, fontWeight: "600" }}>
-            Sign In to your account
-          </Text>
+        <View style={styles.container}>
+          <Text style={styles.labelTitle}>Sign In</Text>
+          <Text style={styles.labelSubtitle}>Sign In to your account</Text>
         </View>
 
-        <View style={{ marginTop: 50 }}>
-          <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <View>
+          <View style={styles.containerInput}>
             <MaterialCommunityIcons
               name="email-outline"
               size={24}
@@ -52,18 +35,11 @@ const LoginScreen = () => {
               value={email}
               onChangeText={(text) => setEmail(text)}
               placeholderTextColor="black"
-              style={{
-                fontSize: email ? 18 : 18,
-                borderBottomWidth: 1,
-                borderBottomColor: "gray",
-                marginLeft: 13,
-                width: 300,
-                marginVertical: 10,
-              }}
+              style={styles.input(email)}
             />
           </View>
 
-          <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <View style={styles.containerInput}>
             <Ionicons name="key-outline" size={24} color="black" />
             <TextInput
               placeholder="Password"
@@ -71,42 +47,16 @@ const LoginScreen = () => {
               onChangeText={(text) => setPassword(text)}
               secureTextEntry
               placeholderTextColor="black"
-              style={{
-                fontSize: password ? 18 : 18,
-                borderBottomWidth: 1,
-                borderBottomColor: "gray",
-                marginLeft: 13,
-                width: 300,
-                marginVertical: 10,
-              }}
+              style={styles.input(email)}
             />
           </View>
 
-          <Pressable
-            style={{
-              width: 200,
-              backgroundColor: "#318CE7",
-              padding: 15,
-              borderRadius: 7,
-              marginTop: 50,
-              marginLeft: "auto",
-              marginRight: "auto",
-            }}
-          >
-            <Text style={{ fontSize: 18, textAlign: "center", color: "white" }}>
-              Login
-            </Text>
+          <Pressable style={styles.btnLogin}>
+            <Text style={styles.labelLogin}>Login</Text>
           </Pressable>
 
-          <Pressable style={{ marginTop: 20 }}>
-            <Text
-              style={{
-                textAlign: "center",
-                fontSize: 17,
-                color: "gray",
-                fontWeight: "500",
-              }}
-            >
+          <Pressable>
+            <Text style={styles.labelRegister}>
               Don't have a account ? Sign Up
             </Text>
           </Pressable>
@@ -118,4 +68,60 @@ const LoginScreen = () => {
 
 export default LoginScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  containerSafeArea: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    padding: 10,
+  },
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 100,
+    marginBottom: 50,
+  },
+  labelTitle: {
+    fontSize: 20,
+    color: "#662d91",
+    fontWeight: "bold",
+  },
+  labelSubtitle: {
+    fontSize: 18,
+    marginTop: 8,
+    fontWeight: "600",
+  },
+  containerInput: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  input: (input) => ({
+    fontSize: input ? 18 : 18,
+    borderBottomWidth: 1,
+    borderBottomColor: "gray",
+    marginLeft: 13,
+    width: 300,
+    marginVertical: 10,
+  }),
+  btnLogin: {
+    width: 200,
+    backgroundColor: "#318CE7",
+    padding: 15,
+    borderRadius: 7,
+    marginTop: 50,
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginBottom: 20,
+  },
+  labelLogin: {
+    fontSize: 18,
+    textAlign: "center",
+    color: "white",
+  },
+  labelRegister: {
+    textAlign: "center",
+    fontSize: 17,
+    color: "gray",
+    fontWeight: "500",
+  },
+});
