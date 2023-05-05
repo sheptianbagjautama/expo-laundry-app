e57fd3afbaeeff01dc4dc13b822ff062eb5a4327
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2023-05-05 10:59:12 +0700

    add animation thums and sparkle, and develop order screen

--- screens/OrderScreen.js
+++ screens/OrderScreen.js
@@ -1,11 +1,50 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import LottieView from "lottie-react-native";
 
 const OrderScreen = () => {
   return (
-    <View>
-      <Text>OrderScreen</Text>
-    </View>
+    <SafeAreaView>
+      <LottieView
+        source={require("../assets/thumbs.json")}
+        style={{
+          height: 360,
+          width: 300,
+          alignSelf: "center",
+          marginTop: 40,
+          justifyContent: "center",
+        }}
+        autoPlay
+        loop={false}
+        speed={0.7}
+      />
+
+      <Text
+        style={{
+          marginTop: 40,
+          fontSize: 19,
+          fontWeight: "600",
+          textAlign: "center",
+        }}
+      >
+        You order has been placed
+      </Text>
+
+      <LottieView
+        source={require("../assets/sparkle.json")}
+        style={{
+          height: 300,
+          position: "absolute",
+          top: 100,
+          width: 300,
+          alignSelf: "center",
+        }}
+        autoPlay
+        loop={false}
+        speed={0.7}
+      />
+    </SafeAreaView>
   );
 };
 
